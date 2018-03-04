a9204eecf37bf9a9955a1c7273b3fbf4dfaf893d
遠山聡美 <S083@INV539noMacBook-Pro.local>
2018-03-04 17:55:27 +0900

    一部レイアウトした

--- 3_NabeatsuApp/ViewController.swift
+++ 3_NabeatsuApp/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tapCountLabel: UILabel!
+    
+    @IBAction func tapCountButton(_ sender: Any) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
